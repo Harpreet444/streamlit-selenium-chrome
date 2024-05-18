54e422e50854640d35bea142fd0e70efe110d3e5
Harpreet444 <145033818+Harpreet444@users.noreply.github.com>
2024-05-18 13:09:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,26 @@
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 
-@st.cache_resource
-def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
 
-options = Options()
-# options.add_argument("--disable-gpu")
-# options.add_argument("--headless")
+    @st.cache_resource
+    def get_driver():
+        return webdriver.Chrome(
+            service=Service(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
 
-driver = get_driver()
-driver.get("http://example.com")
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
 
-st.code(driver.page_source)
+    driver = get_driver()
+    driver.get("http://example.com")
+
+    st.code(driver.page_source)
