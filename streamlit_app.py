ea2cdf9ad3484dec98b5691020fc990bb2bbe2bf
Harpreet444 <145033818+Harpreet444@users.noreply.github.com>
2024-05-18 13:09:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ with st.echo():
         )
 
     options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--headless")
 
     driver = get_driver()
     driver.get("http://example.com")
 
-    st.code(driver.page_source)
+    # st.code(driver.page_source)
